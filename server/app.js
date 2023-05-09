406d4e439edc2b5404b4eb2f5f8d565925c73645
sandip2224 <sandipan2224@gmail.com>
2023-05-09 22:36:58 +0530

    App.js basic express setup

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+require('dotenv').config({ path: './.env' })
+
+const connectDB = require('./helpers/db')
+
+const app = express()
+
+// MongoDB Connection Init
+connectDB(process.env.MONGO_URI)
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// Mounting routes
+app.use('/api/pins', require('./routes/pinRoute'))
+app.use('/api/users', require('./routes/userRoute'))
+
+app.get('/', (req, res) => res.json({ message: 'Travel Mapper API is up and running!' }))
+
+const PORT = process.env.PORT || 3000
+const server = app.listen(PORT, console.log(`🚀 Server running on port ${PORT}!`))
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Server Error: ${err.message}`)
+
+  server.close(() => process.exit(1))
+})
