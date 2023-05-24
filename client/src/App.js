be3f4e4f34cf0a68c9029afff7ed6492b591b11b
sandip2224 <sandipan2224@gmail.com>
2023-05-24 21:09:34 +0530

    Toast + marker

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,51 @@
 import { useEffect, useState } from 'react';
-import Map, { Marker, NavigationControl, Popup } from 'react-map-gl';
-import RoomIcon from '@mui/icons-material/Room';
+import Map, { Marker, Popup } from 'react-map-gl';
 import StarIcon from '@mui/icons-material/Star'
-import StarBorderIcon from '@mui/icons-material/StarBorder';
+import StarBorderIcon from '@mui/icons-material/StarBorder'
+import RoomIcon from '@mui/icons-material/Room'
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import { format } from 'timeago.js'
 import axios from 'axios'
-import './app.css'
 
+import './app.css'
 import 'mapbox-gl/dist/mapbox-gl.css';
+import Register from './components/Register';
 
 function App() {
+  const myStorage = window.localStorage;
+
   const [viewport, setViewport] = useState({
     latitude: 46,
     longitude: 17,
     zoom: 4
   })
-  const currentUser = 'jane'
+
+  const [currentUser, setCurrentUser] = useState(myStorage.getItem('user'));
   const [pins, setPins] = useState([])
   const [currentPlaceId, setCurrentPlaceId] = useState(null)
+  const [newPlace, setNewPlace] = useState(null);
   const [loading, setLoading] = useState(true)
+
+  const [title, setTitle] = useState(null);
+  const [desc, setDesc] = useState(null);
+  const [star, setStar] = useState(0);
+
+  const [showRegister, setShowRegister] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
 
   useEffect(() => {
     const getPins = async () => {
       try {
         const allPins = await axios.get('/pins');
         setPins(allPins.data.pins);
+        toast.success('Fetched all pins!')
         setLoading(false)
       } catch (err) {
         console.log(err);
+        toast.error('Could not fetch pins!')
         setLoading(false);
       }
     };
@@ -36,10 +54,41 @@ function App() {
   }, []);
 
   const handleMarkerClick = (id, lat, lng) => {
-    console.log('Marker clicked!')
     setCurrentPlaceId(id)
-    // setCurrentPlaceId1(id)
+    setViewport({ ...viewport, latitude: lat, longitude: lng });
   }
+
+  const handleAddClick = (e) => {
+    const { lng, lat } = e.lngLat
+    setNewPlace({ lat, lng })
+  }
+
+  const handleLogout = () => {
+    setCurrentUser(null);
+    myStorage.removeItem("user");
+  };
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPin = {
+      username: currentUser,
+      title,
+      desc,
+      rating: star,
+      lat: newPlace.lat,
+      lng: newPlace.lng,
+    };
+
+    try {
+      const res = await axios.post('/pins', newPin);
+      setPins([...pins, res.data.pin]);
+      setNewPlace(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
 
 
   if (loading) return <div className="App">Loading...</div>
@@ -49,6 +98,7 @@ function App() {
       <Map
         {...viewport}
         onMove={evt => setViewport(evt.viewState)}
+        onDblClick={handleAddClick}
         style={{
           width: '100vw',
           height: '100vh'
@@ -64,7 +114,6 @@ function App() {
                 onClick={() => handleMarkerClick(p._id, p.lat, p.lng)}
               />
             </Marker>
-            {console.log(p._id === currentPlaceId)}
             {p._id === currentPlaceId &&
               (<Popup longitude={p.lng} latitude={p.lat} key={p._id} anchor='left'
                 closeButton={true}
@@ -79,11 +128,7 @@ function App() {
                   <label>Rating</label>
                   <label>Information</label>
                   <div className="stars">
-                    <StarIcon className='star' />
-                    <StarIcon className='star' />
-                    <StarIcon className='star' />
-                    <StarIcon className='star' />
-                    <StarIcon className='star' />
+                    {Array(p.rating).fill(<StarIcon className='star' />)}
                   </div>
                   <label>Information</label>
                   <span className='username'>Created by <b>{p.username}</b></span>
@@ -93,8 +138,72 @@ function App() {
             }
           </>
         ))}
-        <NavigationControl />
+        {newPlace &&
+          (<Popup longitude={newPlace.lng} latitude={newPlace.lat} anchor='left'
+            closeButton={true}
+            closeOnClick={false}
+            onClose={() => setNewPlace(null)}
+          >
+            <div>
+              <form onSubmit={handleSubmit}>
+                <label>Title</label>
+                <input
+                  placeholder="Enter a title"
+                  autoFocus
+                  onChange={e => setTitle(e.target.value)}
+                />
+                <label>Description</label>
+                <textarea
+                  placeholder="Say us something about this place."
+                  onChange={e => setDesc(e.target.value)}
+                />
+                <label>Rating</label>
+                <select onChange={e => setStar(e.target.value)}>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                </select>
+                <button type="submit" className="submitButton">
+                  Add Pin
+                </button>
+              </form>
+            </div>
+          </Popup>)
+        }
+        {/* <NavigationControl /> */}
+        {currentUser ? (
+          <button className="button logout" onClick={handleLogout}>
+            Log out
+          </button>
+        ) : (
+          <div className="buttons">
+            <button className="button login" onClick={() => setShowLogin(true)}>
+              Log in
+            </button>
+            <button
+              className="button register"
+              onClick={() => setShowRegister(true)}
+            >
+              Register
+            </button>
+          </div>
+        )}
+        {showRegister && <Register setShowRegister={setShowRegister} />}
+        {showLogin && (
+          <></>
+          // <Login
+          //   setShowLogin={setShowLogin}
+          //   setCurrentUsername={setCurrentUser}
+          //   myStorage={myStorage}
+          // />
+        )}
       </Map >
+      <ToastContainer
+        autoClose={2000}
+        position="bottom-center"
+      />
     </div>
   );
 }
